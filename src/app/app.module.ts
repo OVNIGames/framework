779b9a70ac21d236f7df75d86df1d3d77dd8f852
KyleKatarn <kylekatarnls@gmail.com>
2019-07-24 18:27:32 +0200

    Reorder demo app imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { SocketService } from '../../projects/ovni-games/src/lib/socket.service';
-import { MatProgressSpinnerModule } from '@angular/material';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginModule } from '../../projects/ovni-games/src/lib/login/login.module';
-import { UserModule } from '../../projects/ovni-games/src/lib/user/user.module';
-import { RegisterModule } from '../../projects/ovni-games/src/lib/register/register.module';
 import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
+
 import { GamesListModule } from '../../projects/ovni-games/src/lib/games-list/games-list.module';
 import { GraphQLModule } from '../../projects/ovni-games/src/lib/graphql.module';
+import { LoginModule } from '../../projects/ovni-games/src/lib/login/login.module';
+import { RegisterModule } from '../../projects/ovni-games/src/lib/register/register.module';
+import { SocketService } from '../../projects/ovni-games/src/lib/socket.service';
+import { UserModule } from '../../projects/ovni-games/src/lib/user/user.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
