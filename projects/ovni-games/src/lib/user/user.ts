9bf1c0a7d3d46fcbe19f3983552c5948902f2055
KyleKatarn <kylekatarnls@gmail.com>
2019-07-25 18:32:22 +0200

    Add scope and returned types

--- projects/ovni-games/src/lib/user/user.ts
+++ projects/ovni-games/src/lib/user/user.ts
@@ -1,36 +1,38 @@
-import { IUser } from './user.interface';
-import { IGame } from '../game/game.interface';
 import { Observable, Observer } from 'rxjs';
 import { share } from 'rxjs/operators';
+import { IGame } from '../game/game.interface';
+import { IUser } from './user.interface';
 
 export class User implements IUser {
-  __typename?: string;
-  biography?: string;
-  card_brand?: string;
-  card_last_four?: string;
-  checked?: boolean;
-  created_at?: Date;
-  deleted_at?: Date | null;
-  email?: string;
-  firstname?: string;
-  games?: IGame[];
-  id?: number;
-  language?: string;
-  last_action_at?: Date | null;
-  last_network_event?: Date | null;
-  lastname?: string;
-  name?: string;
-  password?: string;
-  phone?: string;
-  photo_updated_at?: Date | null;
-  sex?: number;
-  stripe_id?: string;
-  timezone?: string;
-  room?: string;
-  trial_ends_at?: Date | null;
-  updated_at?: Date;
+  public __typename?: string;
+  public biography?: string;
+  public card_brand?: string;
+  public card_last_four?: string;
+  public checked?: boolean;
+  public created_at?: Date;
+  public deleted_at?: Date | null;
+  public email?: string;
+  public firstname?: string;
+  public games?: IGame[];
+  public id?: number;
+  public language?: string;
+  public last_action_at?: Date | null;
+  public last_network_event?: Date | null;
+  public lastname?: string;
+  public name?: string;
+  public password?: string;
+  public phone?: string;
+  public photo_updated_at?: Date | null;
+  public sex?: number;
+  public stripe_id?: string;
+  public timezone?: string;
+  public room?: string;
+  public trial_ends_at?: Date | null;
+  public updated_at?: Date;
   private readonly observable: Observable<User>;
-  private observableCallback: () => void = () => {};
+  private observableCallback: () => void = (() => {
+    // noop
+  });
 
   constructor(properties: object, private subscription?: Observer<User>, private mutator?: (user: IUser) => void) {
     this.assign(properties);
@@ -41,32 +43,33 @@ export class User implements IUser {
       };
 
       return () => {
-        this.observableCallback = () => {};
+        this.observableCallback = () => {
+        };
       };
     }).pipe(share());
   }
 
-  getSubscription() {
+  public getSubscription(): Observer<User> | undefined {
     return this.subscription;
   }
 
-  getObservable(): Observable<User> {
+  public getObservable(): Observable<User> {
     return this.observable;
   }
 
-  assign(properties: object) {
-    (<any>Object).assign(this, properties);
+  public assign(properties: object): User {
+    Object.assign(this, properties);
 
     return this;
   }
 
-  extend(properties: IUser) {
+  public extend(properties: IUser): User {
     this.assign(properties).observableCallback();
 
     return this;
   }
 
-  update(properties: IUser) {
+  public update(properties: IUser): User {
     for (const key in properties) {
       if (typeof properties[key] === 'undefined') {
         delete properties[key];
@@ -86,7 +89,7 @@ export class User implements IUser {
     return this;
   }
 
-  kill() {
+  public kill(): User {
     delete this.subscription;
 
     return this;
