89197b24d39dc759aa09f12953c730d46422b714
KyleKatarn <kylekatarnls@gmail.com>
2019-10-16 14:30:02 +0200

    Add IPagination interface

---
+++ projects/ovni-games/src/lib/pagination.interface.ts
@@ -0,0 +1,11 @@
+import { IQuery } from './query.interface';
+
+export interface IPagination<T> extends Partial<IQuery<T>> {
+  total?: number;
+  per_page?: number;
+  last_page?: number;
+  current_page?: number;
+  has_more_pages?: boolean;
+  from?: number;
+  to?: number;
+}
