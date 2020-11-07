8e6744e784ceee0caae0d2f8715399849d3e10fe
KyleKatarn <kylekatarnls@gmail.com>
2020-11-07 23:34:52 +0100

    Implement password change form

--- projects/ovni-games/src/lib/user/user.component.ts
+++ projects/ovni-games/src/lib/user/user.component.ts
@@ -1,4 +1,19 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import { FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FetchResult } from 'apollo-link';
+import { of, throwError } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { ApiService } from '../api.service';
 import { findTimezone, getTimezoneName } from '../timezone-selector/find-timezone';
 import { ITimezone } from '../timezone-selector/timezone.interface';
@@ -14,13 +29,35 @@ import { UserService } from './user.service';
 export class UserComponent implements OnInit, OnChanges {
   public loading = false;
   public editing = false;
+  public changingPassword = false;
+  public typingPassword = true;
+  public typingCurrentPassword = true;
+  public passwordLoading = false;
   public firstName = '';
   public lastName = '';
+  public changePasswordForm = new FormGroup({
+    currentPassword: new FormControl('', [
+      Validators.required,
+    ]),
+    newPassword: new FormControl('', [
+      Validators.required,
+    ]),
+    newPasswordAgain: new FormControl('', [
+      Validators.required,
+    ]),
+  }, (formGroup: FormGroup): ValidationErrors | null => {
+    if (formGroup.get('newPassword')?.value === formGroup.get('newPasswordAgain')?.value) {
+      return null;
+    }
+
+    return { passwordMismatch: true };
+  });
   public timezone?: ITimezone;
   @Input() public user: User;
   @Output() protected userLoggedOut: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @ViewChild('passwordModifier', {static: true}) protected passwordModifier: ElementRef;
 
-  constructor(private userService: UserService, private api: ApiService) {
+  constructor(private userService: UserService, private api: ApiService, private snackBar: MatSnackBar) {
   }
 
   public ngOnInit(): void {
@@ -63,6 +100,14 @@ export class UserComponent implements OnInit, OnChanges {
     this.editing = false;
   }
 
+  public changePassword(): void {
+    this.changingPassword = true;
+  }
+
+  public cancelPasswordChange(): void {
+    this.changingPassword = false;
+  }
+
   public save(): void {
     const properties = {
       firstname: this.firstName,
@@ -77,5 +122,56 @@ export class UserComponent implements OnInit, OnChanges {
     });
     this.user.name = `${this.firstName} ${this.lastName}`;
     this.editing = false;
+  }
+
+  public savePassword(): void {
+    this.changePasswordForm.setErrors({});
+    this.typingPassword = false;
+    this.typingCurrentPassword = false;
+    const controls = this.changePasswordForm.controls;
+
+    if (controls.newPassword.value !== controls.newPasswordAgain.value) {
+      this.changePasswordForm.setErrors({ passwordMismatch: true });
+      controls.newPasswordAgain.setErrors({ passwordMismatch: true });
+
+      return;
+    }
+
+    this.passwordLoading = true;
+
+    this.api.mutate('updateUserPassword', {
+      id: this.user.id,
+      currentPassword: controls.currentPassword.value,
+      password: controls.newPassword.value,
+    }, this.userService.getUserDataFields()).pipe(
+      switchMap((result: FetchResult<{ updateUserPassword: IUser }>) => {
+        if (result?.data?.updateUserPassword) {
+          return of(result.data);
+        }
+
+        const error = {
+          // @ts-ignore
+          message: 'Internal server error',
+          debugMessage: null,
+          ...((result.errors || [])[0] || {}),
+        };
+
+        return throwError(error.debugMessage || error.message)
+      }),
+    ).subscribe(
+      () => {
+        this.passwordLoading = false;
+        this.changingPassword = false;
+        this.changePasswordForm.reset();
+        this.snackBar.open(this.passwordModifier.nativeElement.innerText);
+      },
+      () => {
+        this.passwordLoading = false;
+        setTimeout(() => {
+          this.changePasswordForm.setErrors({ incorrect: true });
+          controls.currentPassword.setErrors({ incorrect: true });
+        });
+      },
+    );
   }
 }
--- projects/ovni-games/src/lib/user/user.module.ts
+++ projects/ovni-games/src/lib/user/user.module.ts
@@ -5,9 +5,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { TimezoneSelectorModule } from '../timezone-selector/timezone-selector.module';
 import { UserComponent } from './user.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
@@ -19,8 +20,10 @@ import { FormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatProgressSpinnerModule,
+    MatSnackBarModule,
 
     TimezoneSelectorModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     UserComponent,
